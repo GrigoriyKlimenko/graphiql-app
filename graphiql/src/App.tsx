f1f8a5a73c141b8e6da60f0e181f9aa2c1072cf3
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-05-11 16:21:49 +0300

    feat: prepare a structure of the project

--- graphiql/src/App.tsx
+++ graphiql/src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Welcome from './pages/welcome/welcome';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import './App.css';
 
-function App() {
-  return <div>Hello world</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ graphiql/src/pages/welcome/welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Welcome = () => {
+  return (
+    <section className="welcome">
+      <div className="container">
+        <h1 className="welcome__title">Welcome page</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
