78617efe5738bd4a2a8461bc00d3fcd03df1b33d
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-05-11 16:36:14 +0300

    feat: realize routing, create 404 page and empty graphiql page

--- graphiql/src/App.tsx
+++ graphiql/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import Welcome from './pages/welcome/welcome';
+import Welcome from './pages/welcome';
+import Graphiql from './pages/graphiql';
+import NotFound from './pages/404';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import './App.css';
@@ -13,6 +15,8 @@ const App = () => {
         <main className="main">
           <Routes>
             <Route path="/" element={<Welcome />} />
+            <Route path="/graphiql" element={<Graphiql />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
