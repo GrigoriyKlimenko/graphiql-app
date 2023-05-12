da785cf6a305f03dfe8a4b0673641c37d0e62bf2
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-05-12 15:37:37 +0300

    feat: add footer and style it

--- graphiql/src/components/footer/footer.tsx
+++ graphiql/src/components/footer/footer.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import './index.css';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container footer__container">
+        <div className="footer-links">
+          <a
+            className="footer-link footer-link__github"
+            href="https://github.com/GrigoriyKlimenko"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="icon_github"></span>
+            Grigoriy Klimenko
+          </a>
+          <a
+            className="footer-link footer-link__github"
+            href="https://github.com/imnephy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="icon_github"></span>Ilya Vylegzhanin
+          </a>
+          <a
+            className="footer-link footer-link__github"
+            href="https://github.com/pshigotskaya26"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="icon_github"></span>Pshihotskaya Tatsiana
+          </a>
+        </div>
+        <p className="footer-text">2023</p>
+        <a
+          className="footer-link footer-link__rsschool"
+          href="https://rs.school/react/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="icon icon_rsschool"></span>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
