854320578d7cb0985614c5598cbd2ef52597d04f
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-05-13 15:30:09 +0300

    feat: implement welcome page and dtyle it

--- graphiql/src/pages/welcome/index.tsx
+++ graphiql/src/pages/welcome/index.tsx
@@ -4,8 +4,34 @@ import './index.css';
 const Welcome = () => {
   return (
     <section className="welcome">
-      <div className="container">
-        <h1 className="welcome__title">Welcome page</h1>
+      <div className="welcome__container container">
+        <div className="welcome__content">
+          <div className="welcome__info">
+            <h1 className="welcome__title">
+              The project <span>"GraphiQL"</span>
+            </h1>
+            <p className="welcome__description">
+              GraphiQL is a playground/IDE for graphQL requests.
+            </p>
+
+            <p className="welcome__course">
+              RS School Course: <span>"React"</span>
+            </p>
+            <div className="welcome__team">
+              <p className="team__text">Our team:</p>
+              <ul className="team-list">
+                <li className="team-list__item">Grigoriy Klimenko</li>
+                <li className="team-list__item">Ilya Vylegzhanin</li>
+                <li className="team-list__item">Pshihotskaya Tatsiana</li>
+              </ul>
+            </div>
+          </div>
+          <div className="welcome__image">
+            <div className="welcome-image">
+              <span className="icon icon_graphiql"></span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
