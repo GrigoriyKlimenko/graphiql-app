579f1417b4ff01d6e2054792dcf266f11e278537
Gregory Klimenko <fageol46@gmail.com>
2023-05-27 16:27:02 +0300

    fix init value of variables

--- graphiql/src/database/database.ts
+++ graphiql/src/database/database.ts
@@ -6,7 +6,7 @@ const DATA = [
   {
     name: 'Film',
     body: `query Film($filmId: ID) {film(filmID: $filmId) {characterConnection{totalCount} created director edited episodeID id openingCrawl planetConnection{totalCount} producers releaseDate speciesConnection{totalCount} starshipConnection{totalCount} title vehicleConnection{totalCount}}}`,
-    variables: `{"filmId": 1}`,
+    variables: `{"filmId": null}`,
   },
   {
     name: 'All people',
@@ -15,7 +15,7 @@ const DATA = [
   {
     name: 'Person',
     body: `query Person($personId: ID) {person(personID: $personId) {birthYear created edited eyeColor filmConnection{totalCount} gender hairColor height homeworld{name} id mass name skinColor species{name} starshipConnection{totalCount} vehicleConnection{totalCount}}}`,
-    variables: `{"personId": 1}`,
+    variables: `{"personId": null}`,
   },
   {
     name: 'All planets',
@@ -24,7 +24,7 @@ const DATA = [
   {
     name: 'Planet',
     body: `query Planet($planetId: ID) {planet(planetID: $planetId)  {climates created diameter edited filmConnection{totalCount} gravity id name orbitalPeriod population residentConnection{totalCount} rotationPeriod surfaceWater terrains}}`,
-    variables: `{"planetId": 1}`,
+    variables: `{"planetId": null}`,
   },
   {
     name: 'All species',
@@ -33,7 +33,7 @@ const DATA = [
   {
     name: 'Species',
     body: `query Species($speciesId: ID) {species(speciesID: $speciesId)  {averageHeight averageLifespan classification created designation edited eyeColors filmConnection{totalCount} hairColors homeworld{name} id language name personConnection{totalCount} skinColors}}`,
-    variables: `{"speciesId": 1}`,
+    variables: `{"speciesId": null}`,
   },
   {
     name: 'All starships',
@@ -42,7 +42,7 @@ const DATA = [
   {
     name: 'Starship',
     body: `query Starships($starshipId: ID) {starship(starshipID: $starshipId) {MGLT cargoCapacity consumables costInCredits created crew edited filmConnection{totalCount} hyperdriveRating id length manufacturers maxAtmospheringSpeed model name passengers pilotConnection{totalCount} starshipClass}}`,
-    variables: `{"starshipId": 2}`,
+    variables: `{"starshipId": null}`,
   },
   {
     name: 'All vehicles',
@@ -51,7 +51,7 @@ const DATA = [
   {
     name: 'Vehicle',
     body: `query Vehicle($vehicleId: ID) {vehicle(vehicleID: $vehicleId) {cargoCapacity consumables costInCredits created crew edited filmConnection{totalCount} id length manufacturers maxAtmospheringSpeed model name passengers pilotConnection{totalCount} vehicleClass}}`,
-    variables: `{"vehicleId": 7}`,
+    variables: `{"vehicleId": null}`,
   },
 ];
 
